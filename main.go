71ad5609aee777015d323ebee4cf6799e48067f2
Noé Tarbouriech <noe.tarbouriech@etu.umontpellier.fr>
2022-12-10 00:21:15 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
+	fmt.Println("Starting server on port 3000")
+	http.ListenAndServe(":3000", r)
+}
