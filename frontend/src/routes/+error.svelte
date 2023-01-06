b7c68fbb04e66bf624bf6c375d76891667706655
Noé Tarbouriech <noe.tarbouriech@etu.umontpellier.fr>
2023-01-06 19:06:25 +0100

    fix(front): fix link for 404 page

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -7,7 +7,7 @@
 </svelte:head>
 
 <Heading align="center">ERROR 404 - NOT FOUND</Heading>
-<Img src="404.gif" size="max-w-2xl" alt="404 image" class="mx-auto mt-16 rounded-lg " />
+<Img src="/404.gif" size="max-w-2xl" alt="404 image" class="mx-auto mt-16 rounded-lg " />
 <A href="/" textColor="text-blue-600 dark:text-blue-500" aClass="flex items-center font-medium hover:underline justify-center mt-6">
     Go back to homepage
     <svg aria-hidden="true" class="ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
